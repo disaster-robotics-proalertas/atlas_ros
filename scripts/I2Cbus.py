89b2506556bfed8fab637aea3464f1901b8b196a
rgmaidana <renanmaidana@gmail.com>
2020-01-21 18:38:50 -0300

    Fixed bugs for calibration procedures

--- scripts/I2Cbus.py
+++ scripts/I2Cbus.py
@@ -6,24 +6,20 @@ import sys
 import time
 
 class I2Cbus:
-	def __init__(self, address=1, timeout=1, **kwargs):
-		self.__address = address
+	def __init__(self, timeout=1, **kwargs):
 		self.__timeout = timeout
-
 		self.bus = smbus.SMBus(1)
 
-	def change_address(self, address):
-		self.__address = int(address)
-
-	def read_line(self):
+	def read_line(self, address):
 		try:
-        		response = self.bus.read_i2c_block_data(self.__address, 0x00)
-        	except IOError as e:
-			print "[Quality] An error", e, "ocurred while reading on address", self.__address
+			response = self.bus.read_i2c_block_data(address, 0x00)
+		except IOError as e:
+			print "[Quality] Error %s occurred while reading on address %d" % (e.strerror, address)
 			return None
-		response = [i for i in response if i != '\00']
-        	char_list = list(map(lambda x: chr(x & ~0x80), list(response[1:])))
-        	return ''.join(char_list).strip("\x00")
+		response = [i for i in response if not i == '\00']
+		char_list = list(map(lambda x: chr(x & ~0x80), list(response[1:])))
+		char_list = ''.join(char_list).strip('\x00').split(',')
+		return [float(x) for x in char_list]
 
 	def convert_string_to_bytes(self, cmd):
         	converted = []
@@ -31,26 +27,24 @@ class I2Cbus:
         		converted.append(ord(b))
 	        return converted
 
-	def send_cmd(self, cmd):
-        	start = ord(cmd[0])
-	        end = cmd[1:] + "\00"
-        	end = self.convert_string_to_bytes(end)
-        	try:
-        		self.bus.write_i2c_block_data(self.__address, start, end)
+	def send_cmd(self, cmd, address):
+		start = ord(cmd[0])
+		end = cmd[1:] + "\00"
+		end = self.convert_string_to_bytes(end)
+		try:
+			self.bus.write_i2c_block_data(address, start, end)
+			time.sleep(self.__timeout)			# Wait 1s for response to arrive
 			return True
-        	except IOError as e:
-        		print "[Quality] An error", e, "ocurred while writing on address", self.__address
+		except IOError as e:
+			print "[Quality] Error %s occurred while writing on address %d" % (e.strerror, address)
 			return None
 
 	def get_data(self, address):
 		"""
 		Gets a single reading from sensor in selected port
 		"""
-		self.change_address(address)
-
 		# Send request for data
-		self.send_cmd("R")
-		time.sleep(self.__timeout)			# Wait 1s for the data to arrive
-		line = self.read_line()
+		self.send_cmd("R", address)
+		line = self.read_line(address)
 		
 		return line
